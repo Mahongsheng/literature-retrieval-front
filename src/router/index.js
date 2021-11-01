3add7b03f99feac7dbe698c7f4ace2cef75131bd
Zhou Yuxiang <zyx1999@users.noreply.github.com>
2021-11-01 18:58:59 +0800

    1.1

--- src/router/index.js
+++ src/router/index.js
@@ -7,14 +7,14 @@ const routes = [
     name: 'Home',
     component: Home
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // },
   {
     path: '/search',
     name: 'Search',
